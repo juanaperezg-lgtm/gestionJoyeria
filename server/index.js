d00ffe2c712ae89059b6679608ffae2d0a23b6c6
PereZzZzZ580 <114631210+PereZzZzZ580@users.noreply.github.com>
2026-05-10 09:14:54 -0500

    feat: initialize express server with API routing and keep-alive mechanism

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use('/api/clients', authMiddleware, require('./routes/clients'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../dist')));
 
-  app.get('/:path(.*)', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
 }
